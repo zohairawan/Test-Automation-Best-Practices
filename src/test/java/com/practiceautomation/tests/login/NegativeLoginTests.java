e22ca517969155026d19e78c34e5126b990459f4
zohairawan <35776095+zohairawan@users.noreply.github.com>
2025-01-07 22:37:25 -0800

    Create incorrectUsernameTest() and incorrectPasswordTest() in NegativeLoginTests class along with @Test

--- src/test/java/com/practiceautomation/tests/login/NegativeLoginTests.java
+++ src/test/java/com/practiceautomation/tests/login/NegativeLoginTests.java
@@ -1,4 +1,16 @@
 package com.practiceautomation.tests.login;
 
+import org.testng.annotations.Test;
+
 public class NegativeLoginTests {
+
+    @Test
+    public void incorrectUsernameTest() {
+
+    }
+
+    @Test
+    public void incorrectPasswordTest() {
+
+    }
 }
