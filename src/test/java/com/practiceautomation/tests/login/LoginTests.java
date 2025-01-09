102e4246b85764b351928220a1dee0d11b225889
zohairawan <35776095+zohairawan@users.noreply.github.com>
2025-01-08 22:23:36 -0800

    Refactor all tests into one class called LoginTests

--- src/test/java/com/practiceautomation/tests/login/LoginTests.java
+++ src/test/java/com/practiceautomation/tests/login/LoginTests.java
@@ -7,7 +7,37 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class NegativeLoginTests {
+public class LoginTests {
+
+    @Test
+    public void testLoginFunctionality() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        String url = "https://practicetestautomation.com/practice-test-login/";
+        driver.get(url);
+
+        WebElement usernameInputField = driver.findElement(By.id("username"));
+        usernameInputField.sendKeys("student");
+
+        WebElement passwordInputField = driver.findElement(By.id("password"));
+        passwordInputField.sendKeys("Password123");
+
+        WebElement submitButton = driver.findElement(By.id("submit"));
+        submitButton.click();
+
+        String expectedUrl = "practicetestautomation.com/logged-in-successfully/";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertTrue(actualUrl.contains(expectedUrl));
+
+        String expectedSuccessMessage = "successfully logged in";
+        String actualSucessMessage = driver.findElement(By.tagName("strong")).getText();
+        Assert.assertTrue(actualSucessMessage.contains(expectedSuccessMessage));
+
+        WebElement logoutButton = driver.findElement(By.linkText("Log out"));
+        Assert.assertTrue(logoutButton.isDisplayed());
+
+        driver.quit();
+    }
 
     @Test
     public void incorrectUsernameTest() {
